0417c7b074e7785f4275dce63512d374160a44e3
Yonatan Elhanan <yonatan.elhanan@gmail.com>
2020-06-26 19:03:56 +0300

    Implement tagged-fields with bytegeist map-of

--- src/skazka/protocol.clj
+++ src/skazka/protocol.clj
@@ -4,46 +4,29 @@
 (def compact-string
   "String with unsigned varint length delimiter set to N+1 (N is number of bytes).
   N=0 means empty string \"\". N=-1 means nil"
-  (g/spec [:string :uvarint32 1]))
+  (g/spec [:string {:length :uvarint32 :adjust 1}]))
 
 (defn compact-array
   "Array with unsigned varint length delimiter set to N+1 (N is number of items).
   N=0 means empty vector `[]`. N=-1 means nil"
   [s]
-  (g/spec [:vector :uvarint32 s 1]))
+  (g/spec [:vector {:length :uvarint32 :adjust 1} s]))
 
-(defn tagged-fields-spec
-  [& [known-fields]]
-  (let [s-num-fields (g/spec :uvarint32)
-        offset 1]
-    (reify
-      g/Spec
-      (read [_ b]
-        (let [num-fields (- (g/read s-num-fields b) offset)]
-          (if (< num-fields 0)
-            nil
-            (throw (Exception. "Not yet supporting non-nil tagged-fields")))))
-      (write [_ b v]
-        (if (nil? v)
-          (g/write s-num-fields b (dec offset))
-          (throw (Exception. "Not yet supporting non-nil tagged-fields")))))))
-
-(def tagged-fields (tagged-fields-spec))
+(def tagged-fields
+  (g/spec [:map-of {:length :uvarint32} :uvarint32 [:bytes {:length :uvarint32}]]))
 
 (def frame-length
   (g/spec :int32))
 
 (def req-header+
-  (g/spec [:map
-           [:frame-length frame-length]
+  (g/spec [:map {:length frame-length}
            [:api-key :int16]
            [:api-ver :int16]
            [:correlation-id :int32]
-           [:client-id [:string :short]]]))
+           [:client-id [:string {:length :short}]]]))
 
 (def res-header+
-  (g/spec [:map
-           [:frame-length frame-length]
+  (g/spec [:map {:length frame-length}
            [:correlation-id :int32]
            [:tagged-fields tagged-fields]]))
 
@@ -79,7 +62,7 @@
            [:tagged-fields tagged-fields]]))
 
 (def metadata-res-v9
-  (g/spec [:map {:length-based-frame frame-length}
+  (g/spec [:map {:length frame-length}
            [:correlation-id :int32]
            [:header-tagged-fields tagged-fields]
            [:throttle-time-ms :int32]
